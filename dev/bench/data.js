01de6ccfc60512fcfd148a9ea624d2bddbbd6166
github-action-benchmark <github@users.noreply.github.com>
2026-07-11 14:56:16 +0000

    add Benchmark (pytest) benchmark result for cbf58e243070d119f977bb4997005321ce91940c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783781679838,
+  "lastUpdate": 1783781776702,
   "repoUrl": "https://github.com/dorel14/whoosh-ng",
   "entries": {
     "Benchmark": [
@@ -58,6 +58,35 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000027245399659380556",
             "extra": "mean: 945.0974735849868 usec\nrounds: 530"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "david.orel@sfr.fr",
+            "name": "David Orel"
+          },
+          "committer": {
+            "email": "david.orel@sfr.fr",
+            "name": "David Orel"
+          },
+          "distinct": true,
+          "id": "cbf58e243070d119f977bb4997005321ce91940c",
+          "message": "fix(workflows): 🐛 Remplace l'utilisation de `actions/delete-artifact` par un script personnalisé pour supprimer les artefacts obsolètes\n\n* Améliore la gestion des artefacts en utilisant `actions/github-script` pour supprimer les artefacts nommés `github-pages`.\n* Permet une meilleure flexibilité et contrôle sur la suppression des artefacts.",
+          "timestamp": "2026-07-11T16:55:29+02:00",
+          "tree_id": "5344a3891fbdd6d0d093f11d778c012c6358cdff",
+          "url": "https://github.com/dorel14/whoosh-ng/commit/cbf58e243070d119f977bb4997005321ce91940c"
+        },
+        "date": 1783781776108,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmark_async.py::test_run_sync_overhead",
+            "value": 1046.0299343372835,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003723217718661146",
+            "extra": "mean: 955.9955859519011 usec\nrounds: 541"
           }
         ]
       }
