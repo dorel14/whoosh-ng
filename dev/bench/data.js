15ff65c6fcd17c90dad51cbad569fdc1e188c857
github-action-benchmark <github@users.noreply.github.com>
2026-07-11 14:50:02 +0000

    add Benchmark (pytest) benchmark result for 1ee6599ac17ad9719d8f6771b46fc0768c3e1a1b

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783781402404,
+  "repoUrl": "https://github.com/dorel14/whoosh-ng",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "david.orel@sfr.fr",
+            "name": "David Orel"
+          },
+          "committer": {
+            "email": "david.orel@sfr.fr",
+            "name": "David Orel"
+          },
+          "distinct": true,
+          "id": "1ee6599ac17ad9719d8f6771b46fc0768c3e1a1b",
+          "message": "chore(workflows): 🔄 Ajoute la permission `workflows: write` pour permettre les modifications des workflows\n\n* Cette modification est nécessaire pour permettre le déploiement des changements dans les workflows.",
+          "timestamp": "2026-07-11T16:49:14+02:00",
+          "tree_id": "f950a42e37e04850702f79742af69bafcfb876d5",
+          "url": "https://github.com/dorel14/whoosh-ng/commit/1ee6599ac17ad9719d8f6771b46fc0768c3e1a1b"
+        },
+        "date": 1783781402170,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmark_async.py::test_run_sync_overhead",
+            "value": 1130.6109018266484,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004312342106325787",
+            "extra": "mean: 884.4775849802708 usec\nrounds: 506"
+          }
+        ]
+      }
+    ]
+  }
+}
