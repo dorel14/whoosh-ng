28039a0541ba100084944486b3513533f1a21702
semantic-release <semantic-release>
2026-08-07 23:07:54 +0000

    chore(release): v4.2.0 [skip ci]
    Automatically generated by python-semantic-release.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -29,8 +29,8 @@ from __future__ import annotations
 
 import re
 
-__version__ = (4, 1, 0)
-__version_string__ = "4.1.0"
+__version__ = (4, 2, 0)
+__version_string__ = "4.2.0"
 
 
 def versionstring(build=True, extra=True):
